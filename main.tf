5e08756532c4a20b6e8fcd586d1fef0527bafe03
Umesh <nakamume@gmail.com>
2020-07-21 11:31:04 +0900

    removing RFI LFI filter
    Removing the filter for `Query string contains: "://" after decoding as URL.`
    
    It's very common nowadays to put a `url` for purposes like `redirect` or `iss`(issuer by auth providers) in the query params.
    This filter is generating lots of false positives

--- main.tf
+++ main.tf
@@ -307,16 +307,6 @@ resource "aws_wafregional_byte_match_set" "owasp_04_paths_string_set" {
   }
 
   byte_match_tuples {
-    text_transformation   = "URL_DECODE"
-    target_string         = "://"
-    positional_constraint = "CONTAINS"
-
-    field_to_match {
-      type = "QUERY_STRING"
-    }
-  }
-
-  byte_match_tuples {
     text_transformation   = "HTML_ENTITY_DECODE"
     target_string         = "://"
     positional_constraint = "CONTAINS"
@@ -1098,16 +1088,6 @@ resource "aws_waf_byte_match_set" "owasp_04_paths_string_set" {
 
     field_to_match {
       type = "URI"
-    }
-  }
-
-  byte_match_tuples {
-    text_transformation   = "URL_DECODE"
-    target_string         = "://"
-    positional_constraint = "CONTAINS"
-
-    field_to_match {
-      type = "QUERY_STRING"
     }
   }
 
